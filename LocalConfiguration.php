4d0130a023332c23efd9ee656a83cbbcea3c68e6
Stephan Vits <stephanvits@Stephans-MacBook-Pro.local>
2021-03-10 15:32:04 +0630

    pagetsconfig backend layout added to extension sitepackage

--- LocalConfiguration.php
+++ LocalConfiguration.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'BE' => [
-        'debug' => true,
+        'debug' => false,
         'explicitADmode' => 'explicitAllow',
-        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$WlZzT0tYOE9CcXpBaThsWA$D2fq/qFf2GzzH4w3CGqQZhlYNkCo3PLG7W1BNhVZBzE',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$ZHhLMG5oWnZBNXd1QWw0YQ$v1cz/LTF6JIDhJpiJRDIMdvJgFiNJBL8kr5m7r+qAbE',
         'loginSecurityLevel' => 'normal',
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
@@ -29,8 +29,9 @@ return [
     ],
     'EXT' => [
         'extConf' => [
-            'backend' => 'a:6:{s:9:"loginLogo";s:0:"";s:19:"loginHighlightColor";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}',
+            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
             'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
+            'phpmyadmin' => 'a:4:{s:12:"hideOtherDBs";s:1:"1";s:9:"uploadDir";s:21:"uploads/tx_phpmyadmin";s:10:"allowedIps";s:0:"";s:12:"useDevIpMask";s:1:"0";}',
         ],
     ],
     'EXTENSIONS' => [
@@ -45,6 +46,12 @@ return [
         'extensionmanager' => [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
+        ],
+        'phpmyadmin' => [
+            'allowedIps' => '',
+            'hideOtherDBs' => '1',
+            'uploadDir' => 'uploads/tx_phpmyadmin',
+            'useDevIpMask' => '0',
         ],
     ],
     'FE' => [
@@ -71,7 +78,7 @@ return [
                     'writerConfiguration' => [
                         5 => [
                             'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                'disabled' => false,
+                                'disabled' => true,
                             ],
                         ],
                     ],
@@ -88,16 +95,16 @@ return [
         'transport_smtp_username' => '',
     ],
     'SYS' => [
-        'devIPmask' => '*',
-        'displayErrors' => 1,
+        'devIPmask' => '',
+        'displayErrors' => 0,
         'encryptionKey' => '2268521f6d6bb8b7cd6719b550e5e45c22b72c39ad12080e0e93fabd850cf35037c2f38b5a7f8a19a66296b9ea4134c1',
-        'exceptionalErrors' => 12290,
+        'exceptionalErrors' => 4096,
         'features' => [
             'newTranslationServer' => true,
             'unifiedPageTranslationHandling' => true,
         ],
         'sitename' => 'Yangon',
-        'systemLogLevel' => 0,
+        'systemLogLevel' => 2,
         'systemMaintainers' => [
             1,
         ],
--- ext/site_yangon/Configuration/TCA/Overrides/pages.php
+++ ext/site_yangon/Configuration/TCA/Overrides/pages.php
@@ -1,1 +1,6 @@
 <?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'site_yangon',
+    'Configuration/TSconfig/Page/siteYangonTSconfigFile.tsconfig',
+    'Yangon Sitepackage config'
+);
